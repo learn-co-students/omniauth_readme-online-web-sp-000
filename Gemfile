d5b72db928f75db66f51e32368a2d6d79bbcea74
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-21 03:49:02 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'omniauth'
 gem 'omniauth-github'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-
+gem 'dotenv-rails'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, 'eb82d7b6d85660550211', '4f27d895d089771c992d9b58ea89bc0903093d34'
+  provider :github, ENV['CLIENT_ID'], ENV['CLIENT_SECRET']
 end
