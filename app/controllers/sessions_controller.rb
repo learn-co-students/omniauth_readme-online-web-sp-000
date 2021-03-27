e1b6eb5700e14889fbab99c6a45dd364b222f6e7
Chad Lengefeld <chadlengefeld@gmail.com>
2021-03-27 04:39:47 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,17 @@ class SessionsController < ApplicationController
       u.name = auth['info']['name']
       u.email = auth['info']['email']
       u.image = auth['info']['image']
+
     end
 
+  # binding.pry
     session[:user_id] = @user.id
 
     render 'welcome/home'
   end
 
   private
- 
+
   def auth
     request.env['omniauth.auth']
   end
